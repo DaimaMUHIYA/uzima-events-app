9c870ca2b38a6599ab784af260cf9dc4c0846dbf
Daima Muhiya <134271384+DaimaMUHIYA@users.noreply.github.com>
2024-04-28 18:56:59 +0100

    Modification coté style

--- src/components/GestionEvent/GestionEvent.jsx
+++ src/components/GestionEvent/GestionEvent.jsx
@@ -5,12 +5,12 @@ function GestionEvent() {
   return (
     <div className="GestionEvent">
       <div className="container">
-        <div className="column-2">
+        <div className="content-img">
           <img className="img-planner-2" src="src/assets/Planner2.png" alt="" />
         </div>
-        <div className="column-1">
-          <h1 className="text-1">Facilitez la gestion de vos événements</h1>
-          <p className="text-2">
+        <div className="content">
+          <p className="heading">Facilitez la gestion de vos événements</p>
+          <p className="text">
             Avec Uzima Events, vous pouvez facilement créer, modifier, afficher
             et supprimer des événements. Simplifiez votre processus de
             planification et de gestion des événements.
